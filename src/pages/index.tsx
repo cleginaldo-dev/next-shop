024ab8feff35b4bb0fa19984a9bcab48b2319b01
Cleginaldo <cleginaldo3000@gmail.com>
2022-11-30 14:43:23 -0300

    feat: create carousel to home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
 import Image from 'next/image';
 
 import camiseta1 from '../assets/Shirt/1.png';
 import camiseta2 from '../assets/Shirt/2.png';
-// import camiseta3 from '../assets/Shirt/3.png';
-// import camiseta4 from '../assets/Shirt/4.png';
+import camiseta3 from '../assets/Shirt/3.png';
+import camiseta4 from '../assets/Shirt/4.png';
 import { HomeContainer, Product } from '../styles/pages/home';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
         <footer>
@@ -18,8 +27,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta x</strong>
