71d08bf127ab2781d5b5ec01c3254ffec8536b48
Cleginaldo <cleginaldo3000@gmail.com>
2022-11-30 10:35:14 -0300

    feat: add styles and images for home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
-import { styled } from '../styles';
+import Image from 'next/image';
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-});
+import camiseta1 from '../assets/Shirt/1.png';
+import camiseta2 from '../assets/Shirt/2.png';
+// import camiseta3 from '../assets/Shirt/3.png';
+// import camiseta4 from '../assets/Shirt/4.png';
+import { HomeContainer, Product } from '../styles/pages/home';
 
 export default function Home() {
-  return <Button>Enviar</Button>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
