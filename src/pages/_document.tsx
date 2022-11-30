dad2510a0fe0645b1d8642db102d87f003830247
Cleginaldo <cleginaldo3000@gmail.com>
2022-11-30 07:55:59 -0300

    feat: add firt commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/no-danger */
+import { Head, Html, Main, NextScript } from 'next/document';
+
+import { getCssText } from '../styles';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
+      <body className="bg-gray-900 bg-app bg-no-repeat bg-cover">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from '../styles';
+
+const Button = styled('button', {
+  backgroundColor: '$green300',
+});
+
+export default function Home() {
+  return <Button>Enviar</Button>;
+}
