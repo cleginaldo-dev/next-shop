d86afbe4a834c068d9c82dc5bd4f255ba43f0d39
Cleginaldo <cleginaldo3000@gmail.com>
2022-12-09 22:31:59 -0300

    feat: add styles to success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link';
+
+import { ImageContainer, SuccessContainer } from '../styles/pages/success';
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Cleginaldo</strong>, sua <strong>Camiseta</strong> está a
+        caminho da sua casa.
+      </p>
+
+      <Link href="/">voltar ao catalogo</Link>
+    </SuccessContainer>
+  );
 }
